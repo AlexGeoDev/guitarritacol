0558222973dc602669fa9aa9f4fc447b0427d2c9
alexgeodev <jaabec@gmail.com>
2022-12-21 14:08:59 -0500

    Bio ok

--- pages/bio.tsx
+++ pages/bio.tsx
@@ -38,6 +38,23 @@ const Bio = () => {
               alt="Calor Jimenez playing guitar" />
           </picture>        
         </section>
+
+        <section className={styles.bio_card}>
+          <picture className={styles.bio_picture}>
+            <img 
+              className={styles.bio_img}
+              src="/images/CarlosExperience.png"
+              alt="Calor Jimenez playing guitar" />
+          </picture>
+          <div className={styles.bio_text}>
+            <h3 className={styles.bio_titleCard}>Work and Experience</h3>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Quo, exercitationem consequuntur? Culpa perspiciatis natus nam at. 
+              Magnam placeat quod, expedita nesciunt recusandae exercitationem 
+              perspiciatis vitae pariatur vero cupiditate voluptas in?</p>
+          </div>          
+        </section>
       </div>
     </>
   )
