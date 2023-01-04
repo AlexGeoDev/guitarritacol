00c5d2840fd765848aaba41f3646cfc30e960f3f
alexgeodev <jaabec@gmail.com>
2023-01-03 19:50:21 -0500

    Media ok, responsive very CHAMBON

--- components/video/YoutubeEmbed.tsx
+++ components/video/YoutubeEmbed.tsx
@@ -1,14 +1,73 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import styles from '../../pages/media.module.css';
 
-const YoutubeEmbed = ({ embedId }) => {
-  <div className='video-resposive'>
-    {/* <iframe 
-      width={256}
-      height={'auto'}
-      src={``}
-    />  */}
+const YoutubeEmbed = ({ embedId }) => (
+  <div className={styles.video_resposive}>
+    <iframe 
+      className='player'
+      width={'100%'}
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder='0'
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+    <style jsx>{`
+      .player {
+        height: 180px;
+      }
+      @media (min-width: 375px) {
+        .player {
+          height: 211px;
+        }
+      }
+      @media (min-width: 425px) {
+        .player {
+          height: 239px;
+        }
+      }
+      @media (min-width: 500px) {
+        .player {
+          height: 281px;
+        }
+      }
+      @media (min-width: 650px) {
+        .player {
+          height: 365px;
+        }
+      }
+      @media (min-width: 768px) {
+        .player {
+          height: 390px;
+        }
+      }
+      @media (min-width: 900px) {
+        .player {
+          height: 405px;
+        }
+      }
+      @media (min-width: 1024px) {
+        .player {
+          height: 460px;
+        }
+      }
+      @media (min-width: 1200px) {
+        .player {
+          height: 539px;
+        }
+      }
+      @media (min-width: 1440px) {
+        .player {
+          height: 647px;
+        }
+      }
+    `}</style>
   </div>
-}
+);
 
-export default YoutubeEmbed
+YoutubeEmbed.propTypes = {
+  embedId: PropTypes.string.isRequired
+};
+
+export default YoutubeEmbed;
--- pages/media.tsx
+++ pages/media.tsx
@@ -1,58 +1,66 @@
-// import ReactPlayer from 'react-player';
+import YoutubeEmbed from '../components/video/YoutubeEmbed';
 import styles from './media.module.css';
-// import second from 'next-videos'
 
 const media = () => {
   return (
     <>
       <div className={styles.media}>
-        <h2>My Concerts</h2>
+        <h2 className={styles.title}>My Concerts</h2>
         <div className={styles.media_card}>
-          <h3 className={styles.mediaCard_title}>Name of concert</h3>
-
-          <div>
-            <video src={('watch?v=ElF4Kqy3GRM')}></video>
+          <div className={styles.media_card_videoContainer}>
+            <div className={styles.mediaCard_video}>
+            <YoutubeEmbed embedId='f6FUQUf8wlw' />
+            </div>
           </div>
-          
-          <div className={styles.mediaCard_video}>
-            {/* <ReactPlayer 
-              url='https://www.youtube.com/watch?v=pLBuFxMYkx8'
-              width='80vw'
-              height='auto'
-            /> */}
-          </div>
+          <div className={styles.media_card__description}>
+            <h3 className={styles.mediaCard_title}>
+              Name of concert
+            </h3>
 
-          <p className={styles.mediaCard_p}>
-            Description of video
-            
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Velit iusto temporibus perspiciatis! Tempore modi voluptates, 
-            illum fugiat dolores facere nam! Eos non voluptate temporibus 
-            quas, molestiae qui rerum voluptates esse.
+            <p className={styles.mediaCard_p}>
+              Description of video.
+              <br />              
+              Mega concierto en Alemania, fue mero gentio y se armo tremendo pogo.
+              Las gruppies estaba very hot, en fin todo un rockstar ese Carlangas
             </p>
+          </div>
         </div>
 
-        <div className={styles.media_card}>
-          <h2 className={styles.mediaCard_title}>Name of concert</h2>
-          
+        <div className={styles.media_card}>          
           <div className={styles.mediaCard_video}>
-            {/* <ReactPlayer 
-              url='https://www.youtube.com/watch?v=pLBuFxMYkx8'
-              width='80vw'
-              height='auto'
-              controls
-            /> */}
+            <YoutubeEmbed embedId='CwSyKAtk9gI' />
           </div>
+          <div className={styles.media_card__description}>
+            <h3 className={styles.mediaCard_title}>
+              Name of concert
+            </h3>
 
-          <p className={styles.mediaCard_p}>
-            Description of video
-            
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Velit iusto temporibus perspiciatis! Tempore modi voluptates, 
-            illum fugiat dolores facere nam! Eos non voluptate temporibus 
-            quas, molestiae qui rerum voluptates esse.
+            <p className={styles.mediaCard_p}>
+              Description of video.
+              <br />              
+              Mega concierto en Alemania, fue mero gentio y se armo tremendo pogo.
+              Las gruppies estaba very hot, en fin todo un rockstar ese Carlangas
             </p>
+          </div>
         </div>
+
+        <div className={styles.media_card}>          
+          <div className={styles.mediaCard_video}>
+            <YoutubeEmbed embedId='f6FUQUf8wlw' />
+          </div>
+          <div className={styles.media_card__description}>
+            <h3 className={styles.mediaCard_title}>
+              Name of concert
+            </h3>
+
+            <p className={styles.mediaCard_p}>
+              Description of video.
+              <br />              
+              Mega concierto en Alemania, fue mero gentio y se armo tremendo pogo.
+              Las gruppies estaba very hot, en fin todo un rockstar ese Carlangas
+            </p>
+          </div>
+        </div>        
       </div>
     </>
   )
